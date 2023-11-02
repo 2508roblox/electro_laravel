79d5c59800c39077326b8a6af3258a6a22caeced
2508roblox <trangiangzxc@gmail.com>
2023-11-02 17:02:31 +0700

    aaa

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,9 @@ use App\Http\Controllers\Admin\ProductColorController;
 |a
 a
 a
+a
+a
+a
 */
 Route::group(['prefix' => 'auth'], function () {
     Route::middleware(['guest'])->group(function () {
