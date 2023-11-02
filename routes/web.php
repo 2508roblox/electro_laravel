90c3da668512f39a6beb11d05f877be55b70f914
2508roblox <trangiangzxc@gmail.com>
2023-11-02 17:19:49 +0700

    test_2

--- routes/web.php
+++ routes/web.php
@@ -34,12 +34,6 @@ use App\Http\Controllers\Admin\ProductColorController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|a
-a
-a
-a
-a
-a
 */
 Route::group(['prefix' => 'auth'], function () {
     Route::middleware(['guest'])->group(function () {
