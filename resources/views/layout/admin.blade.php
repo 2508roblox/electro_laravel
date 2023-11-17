de80174b0d9011d241a13f68a51f88f64d4774d9
2508roblox <trangiangzxc@gmail.com>
2023-11-17 07:13:36 +0700

    fix_ngrok_request

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charSet="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
 
 
