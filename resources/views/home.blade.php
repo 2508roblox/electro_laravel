c5129442fbc645d18e3f8b317851df24ffe3eaf3
2508roblox <trangiangzxc@gmail.com>
2023-11-18 21:59:17 +0700

    change_cover_images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,12 +72,12 @@ curl_close($ch);
             <div class="row">
                 <div class="col-md-6 mb-4 mb-xl-0">
                     <a href="../shop/shop.html" class="d-block">
-                        <img class="img-fluid" src="{{asset('client/img/690X150/img1.jpg')}}" alt="Image Description">
+                        <img class="img-fluid" src="{{asset('client/img/47.png')}}" style="" alt="Image Description">
                     </a>
                 </div>
                 <div class="col-md-6 mb-4 mb-xl-0">
                     <a href="../shop/shop.html" class="d-block">
-                        <img class="img-fluid" src="{{asset('client/img/690X150/img2.jpg')}}" alt="Image Description">
+                        <img class="img-fluid" src="{{asset('client/img/49.png')}}" style="" alt="Image Description">
                     </a>
                 </div>
             </div>
