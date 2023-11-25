f10e3073eabf8fad4c0526b87f86cdafb065296e
Huyh Huyhu <2509roblox@gmail.com>
2023-11-25 19:56:46 +0700

    fix checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -103,8 +103,7 @@ class CheckoutController extends Controller
      */
     public function store(Request $request)
     {
-
-        // logic
+        // Kiểm tra dữ liệu đầu vào
         $validator = Validator::make($request->all(), [
             'firstname' => 'required|max:255',
             'lastname' => 'required|max:255',
@@ -118,9 +117,15 @@ class CheckoutController extends Controller
             'payment_mode' => 'required|max:255',
         ]);
 
+        // Kiểm tra nếu có lỗi
         if ($validator->fails()) {
-            return redirect()->back()->withErrors($validator)->withInput();
+
+            return redirect()->back()->withErrors($validator);
         }
+
+
+
+
         // Tạo mới order
         $order = new Order;
 
@@ -345,108 +350,108 @@ class CheckoutController extends Controller
 
         $order = Order::find($id);
         $vnp_Url = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-            $vnp_Returnurl = "http://localhost:8000/checkpayment";
-            $vnp_TmnCode = "R3E63P5P"; //Mã website tại VNPAY
-            $vnp_HashSecret = "GXDEHIEBSREFTEALNKYBXMKDKVVBEJPC"; //Chuỗi bí mật
+        $vnp_Returnurl = "http://localhost:8000/checkpayment";
+        $vnp_TmnCode = "R3E63P5P"; //Mã website tại VNPAY
+        $vnp_HashSecret = "GXDEHIEBSREFTEALNKYBXMKDKVVBEJPC"; //Chuỗi bí mật
 
-            $vnp_TxnRef = $order->id; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
-            $vnp_OrderInfo = 'Thanh Toán đơn hàng tại Electro';
-            $vnp_OrderType = 'bank';
-            $vnp_Amount = ($order->shipping_price + $order->total_amount == 0.0 ? 0 : $order->shipping_price + $order->total_amount) * 100 * 24305;
-            $vnp_Locale = 'vn';
-            $vnp_BankCode = 'NCB';
-            $vnp_IpAddr = 'http://localhost:8000/checkpayment';
-            //Add Params of 2.0.1 Version
-            // $vnp_ExpireDate = $_POST['txtexpire'];
-            // //Billing
-            // $vnp_Bill_Mobile = $_POST['txt_billing_mobile'];
-            // $vnp_Bill_Email = $_POST['txt_billing_email'];
-            // $fullName = trim($_POST['txt_billing_fullname']);
-            // if (isset($fullName) && trim($fullName) != '') {
-            //     $name = explode(' ', $fullName);
-            //     $vnp_Bill_FirstName = array_shift($name);
-            //     $vnp_Bill_LastName = array_pop($name);
-            // }
-            // $vnp_Bill_Address=$_POST['txt_inv_addr1'];
-            // $vnp_Bill_City=$_POST['txt_bill_city'];
-            // $vnp_Bill_Country=$_POST['txt_bill_country'];
-            // $vnp_Bill_State=$_POST['txt_bill_state'];
-            // // Invoice
-            // $vnp_Inv_Phone=$_POST['txt_inv_mobile'];
-            // $vnp_Inv_Email=$_POST['txt_inv_email'];
-            // $vnp_Inv_Customer=$_POST['txt_inv_customer'];
-            // $vnp_Inv_Address=$_POST['txt_inv_addr1'];
-            // $vnp_Inv_Company=$_POST['txt_inv_company'];
-            // $vnp_Inv_Taxcode=$_POST['txt_inv_taxcode'];
-            // $vnp_Inv_Type=$_POST['cbo_inv_type'];
-            $inputData = array(
-                "vnp_Version" => "2.1.0",
-                "vnp_TmnCode" => $vnp_TmnCode,
-                "vnp_Amount" => $vnp_Amount,
-                "vnp_Command" => "pay",
-                "vnp_CreateDate" => date('YmdHis'),
-                "vnp_CurrCode" => "VND",
-                "vnp_IpAddr" => $vnp_IpAddr,
-                "vnp_Locale" => $vnp_Locale,
-                "vnp_OrderInfo" => $vnp_OrderInfo,
-                "vnp_OrderType" => $vnp_OrderType,
-                "vnp_ReturnUrl" => $vnp_Returnurl,
-                "vnp_TxnRef" => $vnp_TxnRef,
-                // "vnp_ExpireDate"=>$vnp_ExpireDate,
-                // "vnp_Bill_Mobile"=>$vnp_Bill_Mobile,
-                // "vnp_Bill_Email"=>$vnp_Bill_Email,
-                // "vnp_Bill_FirstName"=>$vnp_Bill_FirstName,
-                // "vnp_Bill_LastName"=>$vnp_Bill_LastName,
-                // "vnp_Bill_Address"=>$vnp_Bill_Address,
-                // "vnp_Bill_City"=>$vnp_Bill_City,
-                // "vnp_Bill_Country"=>$vnp_Bill_Country,
-                // "vnp_Inv_Phone"=>$vnp_Inv_Phone,
-                // "vnp_Inv_Email"=>$vnp_Inv_Email,
-                // "vnp_Inv_Customer"=>$vnp_Inv_Customer,
-                // "vnp_Inv_Address"=>$vnp_Inv_Address,
-                // "vnp_Inv_Company"=>$vnp_Inv_Company,
-                // "vnp_Inv_Taxcode"=>$vnp_Inv_Taxcode,
-                // "vnp_Inv_Type"=>$vnp_Inv_Type
-            );
+        $vnp_TxnRef = $order->id; //Mã đơn hàng. Trong thực tế Merchant cần insert đơn hàng vào DB và gửi mã này sang VNPAY
+        $vnp_OrderInfo = 'Thanh Toán đơn hàng tại Electro';
+        $vnp_OrderType = 'bank';
+        $vnp_Amount = ($order->shipping_price + $order->total_amount == 0.0 ? 0 : $order->shipping_price + $order->total_amount) * 100 * 24305;
+        $vnp_Locale = 'vn';
+        $vnp_BankCode = 'NCB';
+        $vnp_IpAddr = 'http://localhost:8000/checkpayment';
+        //Add Params of 2.0.1 Version
+        // $vnp_ExpireDate = $_POST['txtexpire'];
+        // //Billing
+        // $vnp_Bill_Mobile = $_POST['txt_billing_mobile'];
+        // $vnp_Bill_Email = $_POST['txt_billing_email'];
+        // $fullName = trim($_POST['txt_billing_fullname']);
+        // if (isset($fullName) && trim($fullName) != '') {
+        //     $name = explode(' ', $fullName);
+        //     $vnp_Bill_FirstName = array_shift($name);
+        //     $vnp_Bill_LastName = array_pop($name);
+        // }
+        // $vnp_Bill_Address=$_POST['txt_inv_addr1'];
+        // $vnp_Bill_City=$_POST['txt_bill_city'];
+        // $vnp_Bill_Country=$_POST['txt_bill_country'];
+        // $vnp_Bill_State=$_POST['txt_bill_state'];
+        // // Invoice
+        // $vnp_Inv_Phone=$_POST['txt_inv_mobile'];
+        // $vnp_Inv_Email=$_POST['txt_inv_email'];
+        // $vnp_Inv_Customer=$_POST['txt_inv_customer'];
+        // $vnp_Inv_Address=$_POST['txt_inv_addr1'];
+        // $vnp_Inv_Company=$_POST['txt_inv_company'];
+        // $vnp_Inv_Taxcode=$_POST['txt_inv_taxcode'];
+        // $vnp_Inv_Type=$_POST['cbo_inv_type'];
+        $inputData = array(
+            "vnp_Version" => "2.1.0",
+            "vnp_TmnCode" => $vnp_TmnCode,
+            "vnp_Amount" => $vnp_Amount,
+            "vnp_Command" => "pay",
+            "vnp_CreateDate" => date('YmdHis'),
+            "vnp_CurrCode" => "VND",
+            "vnp_IpAddr" => $vnp_IpAddr,
+            "vnp_Locale" => $vnp_Locale,
+            "vnp_OrderInfo" => $vnp_OrderInfo,
+            "vnp_OrderType" => $vnp_OrderType,
+            "vnp_ReturnUrl" => $vnp_Returnurl,
+            "vnp_TxnRef" => $vnp_TxnRef,
+            // "vnp_ExpireDate"=>$vnp_ExpireDate,
+            // "vnp_Bill_Mobile"=>$vnp_Bill_Mobile,
+            // "vnp_Bill_Email"=>$vnp_Bill_Email,
+            // "vnp_Bill_FirstName"=>$vnp_Bill_FirstName,
+            // "vnp_Bill_LastName"=>$vnp_Bill_LastName,
+            // "vnp_Bill_Address"=>$vnp_Bill_Address,
+            // "vnp_Bill_City"=>$vnp_Bill_City,
+            // "vnp_Bill_Country"=>$vnp_Bill_Country,
+            // "vnp_Inv_Phone"=>$vnp_Inv_Phone,
+            // "vnp_Inv_Email"=>$vnp_Inv_Email,
+            // "vnp_Inv_Customer"=>$vnp_Inv_Customer,
+            // "vnp_Inv_Address"=>$vnp_Inv_Address,
+            // "vnp_Inv_Company"=>$vnp_Inv_Company,
+            // "vnp_Inv_Taxcode"=>$vnp_Inv_Taxcode,
+            // "vnp_Inv_Type"=>$vnp_Inv_Type
+        );
 
-            if (isset($vnp_BankCode) && $vnp_BankCode != "") {
-                $inputData['vnp_BankCode'] = $vnp_BankCode;
-            }
-            if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
-                $inputData['vnp_Bill_State'] = $vnp_Bill_State;
-            }
+        if (isset($vnp_BankCode) && $vnp_BankCode != "") {
+            $inputData['vnp_BankCode'] = $vnp_BankCode;
+        }
+        if (isset($vnp_Bill_State) && $vnp_Bill_State != "") {
+            $inputData['vnp_Bill_State'] = $vnp_Bill_State;
+        }
 
-            //var_dump($inputData);
-            ksort($inputData);
-            $query = "";
-            $i = 0;
-            $hashdata = "";
-            foreach ($inputData as $key => $value) {
-                if ($i == 1) {
-                    $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
-                } else {
-                    $hashdata .= urlencode($key) . "=" . urlencode($value);
-                    $i = 1;
-                }
-                $query .= urlencode($key) . "=" . urlencode($value) . '&';
-            }
-
-            $vnp_Url = $vnp_Url . "?" . $query;
-            if (isset($vnp_HashSecret)) {
-                $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret); //
-                $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
-            }
-            $returnData = array(
-                'code' => '00', 'message' => 'success', 'data' => $vnp_Url
-            );
-            if (true) {
-                // after payment is completed
-
-                header('Location: ' . $vnp_Url);
-                die();
+        //var_dump($inputData);
+        ksort($inputData);
+        $query = "";
+        $i = 0;
+        $hashdata = "";
+        foreach ($inputData as $key => $value) {
+            if ($i == 1) {
+                $hashdata .= '&' . urlencode($key) . "=" . urlencode($value);
             } else {
-                echo json_encode($returnData);
+                $hashdata .= urlencode($key) . "=" . urlencode($value);
+                $i = 1;
             }
+            $query .= urlencode($key) . "=" . urlencode($value) . '&';
+        }
+
+        $vnp_Url = $vnp_Url . "?" . $query;
+        if (isset($vnp_HashSecret)) {
+            $vnpSecureHash =   hash_hmac('sha512', $hashdata, $vnp_HashSecret); //
+            $vnp_Url .= 'vnp_SecureHash=' . $vnpSecureHash;
+        }
+        $returnData = array(
+            'code' => '00', 'message' => 'success', 'data' => $vnp_Url
+        );
+        if (true) {
+            // after payment is completed
+
+            header('Location: ' . $vnp_Url);
+            die();
+        } else {
+            echo json_encode($returnData);
+        }
     }
 
     /**
